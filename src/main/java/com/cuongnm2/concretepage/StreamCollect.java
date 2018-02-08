4a39e63a51cfe4a48d0174e934c6bb851e223441
CUONG NGO <cuongnm2@smartosc.com>
2018-02-08 13:25:34 +0700

    implement more on 8-2

--- src/main/java/com/cuongnm2/concretepage/StreamCollect.java
+++ src/main/java/com/cuongnm2/concretepage/StreamCollect.java
@@ -2,12 +2,19 @@ package com.cuongnm2.concretepage;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamCollect {
 	public static void main(String[] args) {
+		/*
 		List<Integer> list = Arrays.asList(3,5,6);
 		int sum = list.stream().collect(Collectors.summingInt(i -> i));
 		System.out.println("Sum: "+ sum);
+		*/
+		
+		List<String> list = Arrays.asList("Mukesh", "Vishal", "Amar");
+		String result = list.parallelStream().collect(StringBuilder::new,
+				(response, element) -> response.append(" ").append(element),
+				(response1, response2) -> response1.append(",").append(response2.toString())).toString();
+		System.out.println("Result: " + result);
 	}
 }
