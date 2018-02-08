ee20c04add6609b69adc5b7ecbddc2ec3fa45ee9
CUONG NGO <cuongnm2@smartosc.com>
2018-02-08 16:37:05 +0700

    implement reduce

---
+++ src/main/java/com/cuongnm2/concretepage/ReduceDemo1.java
@@ -0,0 +1,12 @@
+package com.cuongnm2.concretepage;
+
+import java.util.Arrays;
+
+public class ReduceDemo1 {
+	public static void main(String[] args) {
+		int[] array = { 23, 43, 56, 97, 32 };
+		Arrays.stream(array).reduce((x, y) -> x + y).ifPresent(s -> System.out.println(s));
+		Arrays.stream(array).reduce(Integer::sum).ifPresent(s -> System.out.println(s));
+		Arrays.stream(array).reduce(StatisticsUtility::addIntData).ifPresent(s -> System.out.println(s));
+	}
+}
---
+++ src/main/java/com/cuongnm2/concretepage/StatisticsUtility.java
@@ -0,0 +1,7 @@
+package com.cuongnm2.concretepage;
+
+public class StatisticsUtility {
+	public static int addIntData(int num1, int num2) {
+		return num1 + num2;
+	}
+}
